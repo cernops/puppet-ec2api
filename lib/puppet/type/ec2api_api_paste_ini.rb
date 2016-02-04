66d6961c307e133122dafd26079e0214e5467415
Marcos Fermin Lobo <marcos.fermin.lobo@cern.ch>
2016-02-04 08:48:44 +0100

    Remove CERN specifics to prepare for upstream

--- lib/puppet/type/ec2api_api_paste_ini.rb
+++ lib/puppet/type/ec2api_api_paste_ini.rb
@@ -6,41 +6,4 @@ Puppet::Type.newtype(:ec2api_api_paste_ini) do
     desc 'Section/setting name to manage from /etc/ec2api/api-paste.ini'
     newvalues(/\S+\/\S+/)
   end
-
-  newproperty(:value) do
-    desc 'The value of the setting to be defined.'
-    munge do |value|
-      value = value.to_s.strip
-      value.capitalize! if value =~ /^(true|false)$/i
-      value
-    end
-
-    def is_to_s( currentvalue )
-      if resource.secret?
-        return '[old secret redacted]'
-      else
-        return currentvalue
-      end
-    end
-
-    def should_to_s( newvalue )
-      if resource.secret?
-        return '[new secret redacted]'
-      else
-        return newvalue
-      end
-    end
-  end
-
-  newparam(:secret, :boolean => true) do
-    desc 'Whether to use a teigi secret as the value. Defaults to `false`.'
-
-    newvalues(:true, :false)
-
-    defaultto false
-  end
-
-  autorequire(:teigisecret) do
-    self[:value] if self[:secret] == :true
-  end
 end
--- lib/puppet/type/ec2api_config.rb
+++ lib/puppet/type/ec2api_config.rb
@@ -9,43 +9,4 @@ Puppet::Type.newtype(:ec2api_config) do
     desc 'Section/setting name to manage from ec2api.conf'
     newvalues(/\S+\/\S+/)
   end
-
-  newproperty(:value) do
-    desc 'The value of the setting to be defined, or the key to be used in teigi.'
-    unless :secret == true
-      munge do |value|
-        value = value.to_s.strip
-        value.capitalize! if value =~ /^(true|false)$/i
-        value
-      end
-    end
-
-    def is_to_s( currentvalue )
-      if resource.secret?
-        return '[old secret redacted]'
-      else
-        return currentvalue
-      end
-    end
-
-    def should_to_s( newvalue )
-      if resource.secret?
-        return '[new secret redacted]'
-      else
-        return newvalue
-      end
-    end
-  end
-
-  newparam(:secret, :boolean => true) do
-    desc 'Whether to use a teigi secret as the value. Defaults to `false`.'
-
-    newvalues(:true, :false)
-
-    defaultto false
-  end
-
-  autorequire(:teigisecret) do
-    self[:value] if self[:secret] == :true
-  end
 end
